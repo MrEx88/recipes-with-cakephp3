8e0b27a2831353201431495f4f08d4762a3a0436
Jon Wesneski <jon.wesneski@salientsys.com>
2016-06-29 19:59:16 -0500

    Adding query helper class

---
+++ src/View/Helper/QueryHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Helper;
+
+use Cake\View\Helper;
+use Cake\ORM\Query;
+use Cake\ORM\Table;
+use Cake\ORM\TableRegistry;
+
+class QueryHelper extends Helper
+{
+    /**
+     * Gets the tags associated with the recipe id.
+     *
+     * @param $recipeId The id of the recipe.
+     * @return An array of tags
+     */
+    public function getTags($recipeId)
+    {
+        $recipes = TableRegistry::get('Tags');
+        // TODO: Still need to figure out the correct query
+        $query = $recipes->find()
+            ->select(['name'])->from('Tags')
+            ->where(['recipe_tags.recipe_id' => $recipeId]);
+        $query->hydrate(false);
+        
+        return $query->toList();
+    }
+}
